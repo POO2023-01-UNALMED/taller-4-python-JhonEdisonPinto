d74f4c00e4f2f4a7d1a561d44926eb0dc9245152
JhonEdisonPinto <124928317+JhonEdisonPinto@users.noreply.github.com>
2023-03-04 17:15:50 -0500

    tercero
    tercero

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -12,7 +12,7 @@ class Grupo:
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
+    def agregarAlumno(self, alumno, lista=None):
         if lista != None:
             lista.append(alumno)
         else:
